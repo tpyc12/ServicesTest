2b55a9d9fe4e9a0f9991a5b402b0acf1b4b9d61e
tpyc12 <tpyc12@yandex.ru>
2022-04-06 12:55:27 +0300

    Implements coroutine and view binding. Create xml file.

--- app/src/main/java/com/myhome/android/servicestest/MainActivity.kt
+++ app/src/main/java/com/myhome/android/servicestest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.myhome.android.servicestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myhome.android.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
